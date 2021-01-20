73323b0bb098a828e11447561843dad216323b64
DVasileva <74765255+DVasileva@users.noreply.github.com>
2021-01-20 18:29:19 +0200

    action

--- Project-dresses/Input.php
+++ Project-dresses/Input.php
@@ -7,10 +7,10 @@
 	<title>The green dress</title>
 </head>
 <body>
-	<form method="post" action="result.php" >
+	<form method="post" action="script_result.php" >
 		<h2> Welcome to the green color center !</h2>
 		<p>Enter the code you want to check : </p>
-		
+
 		<input type="text" name="code">
 		<input type="submit" name = "submit"  value="Check">
 	</form>
