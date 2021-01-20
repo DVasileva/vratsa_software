47325d80fdce2e2159cc561883bfb41778dc01f5
DVasileva <74765255+DVasileva@users.noreply.github.com>
2021-01-20 16:46:01 +0200

    css_link

---
+++ Project-dresses/Welcome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="widht=device-widht, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+	<title>The green dress</title>
+</head>
+<body>
+	<form method="post" action="result.php" >
+		<h2> Welcome to the green color center !</h2>
+		<p>Enter the code you want to check : </p>
+		
+		<input type="text" name="code">
+		<input type="submit" name = "submit"  value="Check">
+
+
+	</form>
+</body>
+</html>
