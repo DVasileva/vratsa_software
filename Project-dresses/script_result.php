a0b1ad8dca724e12c259fac43da8c31d2599915c
DVasileva <74765255+DVasileva@users.noreply.github.com>
2021-01-22 16:07:02 +0200

    result

--- Project-dresses/script_result.php
+++ Project-dresses/script_result.php
@@ -15,18 +15,32 @@ if (!isset($_POST['code'])) {
 			$new = str_split($value, 2);
 			$new_arr[] = $new;
 		}
-		$hdec = [];
+		$dec = [];
 		$count = count($new_arr);
-		for ($i=0; $i < $count ; $i++) { 
-			$hdec[$i] = [];
-			for ($i=0; $i < 3; $i++) { 
+		for ($i=0; $i < $count; $i++) { 
+			$dec[$i] = [];
+			for ($j=0; $j < 3; $j++) { 
 				foreach ($new_arr as $value) {
-					$val = hexdec($new_arr[$i][$j])
-					$new_arr[$i][$j]= $val;
+	 				$val = hexdec($new_arr[$i][$j]);
+	 				$dec[$i][$j] = $val;
 					
 				}
 			}
 		}
-
+	$search = [];
+		foreach ($dec as $key => $value) {
+			if ($value[1] > $value[0] && $value[1] > $value[2]) {
+				$search[]=$key;
+			}
+		}
+		echo "[";
+        $count1 = count($search);
+        for ($m=0; $m < $count1; $m++) {
+        	echo $search[$m]; 
+        	if ($m!= $count1-1 ) {
+            echo ", ";
+              
+        }  
+	}
+	echo "]";
 }
-?>
